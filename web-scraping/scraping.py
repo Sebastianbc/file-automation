fe10a7cacb3504ccf9e943f2d5ec1d994d044afe
SebastianBC <sebastianbrinezc@gmail.com>
2021-11-23 12:26:17 -0500

    Isolating data

--- web-scraping/scraping.py
+++ web-scraping/scraping.py
@@ -8,4 +8,16 @@ response = requests.get(url)
 
 soup = BeautifulSoup(response.text, 'lxml')
 
-print(soup)
+# Get just the text quote, with its author and its corresponding tags, then print them.
+# Getting all span text classes with their html tags
+quotes = soup.find_all('span', class_='text')
+# Get the quote's author
+authors = soup.find_all('small', class_='author')
+# Get the tags related the quotes
+tags = soup.find_all('div', class_='tags')
+print(tags)
+# Get just the span tag text
+for i in range(0, len(quotes)):
+    print(quotes[i].text, 'by', authors[i].text, tags[i].text)
+    
+#print(quotes)
